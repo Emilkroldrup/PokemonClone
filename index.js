dc5f383d2037d9522c1f6659caf20648cfee822f
Emilkroldrup <Emil@ak-es.dk>
2023-05-10 10:19:27 +0200

    beginning of boundaries

--- index.js
+++ index.js
@@ -5,9 +5,43 @@ const ctx = canvas.getContext('2d')
 // Set the canvas size to the window size
     canvas.width = 1024
     canvas.height = 576
+    
+    const collisionsMap = []
+    for (i = 0; i < collisions.length; i+= 70) {
+        collisionsMap.push(collisions.slice(i, 70 + i))
+    }
 
-    ctx.fillStyle = 'White'
-    ctx.fillRect(0, 0, canvas.width, canvas.height)
+    class Boundary {
+        constructor({position}) {
+            this.position = position
+            this.width = 48
+            this.height = 48
+        }
+
+        draw() {
+            ctx.fillStyle = 'red'
+            ctx.fillRect(this.position.x, this.position.y, this.width, this.height)
+        }
+    }
+
+    const boundaries = []
+    const offset = {
+        x: -750,
+        y: - 600
+    }
+
+    collisionsMap.forEach((row, i) => {
+        row.forEach((symbol, j) => {
+            if (symbol === 1025)
+            boundaries.push(new Boundary({position: {
+                x: j * Boundary.width + offset.x, 
+                y: i * Boundary.height + offset.y
+                 }
+                })
+             )
+        })
+     })
+
 
     const player = new Image()
     player.src = 'images/PlayerDown.png'
@@ -27,10 +61,12 @@ const ctx = canvas.getContext('2d')
         }
     }
 
+    
+
     const background = new Sprite({
         position: {
-            x: -750, 
-            y: -600
+            x: offset.x,
+            y: offset.y
         },
         image: image
     })
@@ -56,6 +92,9 @@ const ctx = canvas.getContext('2d')
  function animate() { 
      window.requestAnimationFrame(animate)
         background.draw(),
+        boundaries.forEach(Boundary => {
+            Boundary.draw
+        })
         ctx.drawImage(
             player,
             0,
